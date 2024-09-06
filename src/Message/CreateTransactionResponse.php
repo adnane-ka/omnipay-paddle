c19be79890a713b7906fc9ce509622293c179926
adnane-ka <dannykadri0@gmail.com>
2024-09-06 21:27:33 +0100

    Refinements

--- src/Message/CreateTransactionResponse.php
+++ src/Message/CreateTransactionResponse.php
@@ -18,14 +18,7 @@ class CreateTransactionResponse extends AbstractResponse implements RedirectResp
     
     /***/
     public function getRedirectUrl(){
-        $transactionId = $this->data['data']['id'];
-        $returnUrl = urldecode($this->data['returnUrl']);
-        $queryString = parse_url($returnUrl, PHP_URL_QUERY) ?? '';  // Get the query string from the URL
-        parse_str($queryString, $queryParams);  // Parse the query string into an array
-        $queryParams['transactionId'] = $transactionId;  // Add the transactionId to the array
-        $fullUrl = $this->data['checkoutUrl'] . '?transactionId=' . $transactionId . '&returnUrl=' . urlencode($returnUrl . '?' . http_build_query($queryParams));
-        
-        return $fullUrl;
+        return $this->buildRedirectUrl();
     }
 
     /***/
@@ -33,5 +26,31 @@ class CreateTransactionResponse extends AbstractResponse implements RedirectResp
         return $this->data['error']['code']
         .': '.$this->data['error']['detail']
         .'. request ID:'.$this->data['meta']['request_id'];
+    }
+
+    public function buildRedirectUrl(){
+        $transactionId = $this->data['data']['id'];
+        $returnUrl = urldecode($this->data['returnUrl']);
+        $checkoutUrl = $this->data['checkoutUrl'];
+    
+        // Parse query strings from both URLs
+        $returnParams = [];
+        $checkoutParams = [];
+    
+        parse_str(parse_url($returnUrl, PHP_URL_QUERY) ?? '', $returnParams);
+        parse_str(parse_url($checkoutUrl, PHP_URL_QUERY) ?? '', $checkoutParams);
+    
+        // Add transactionId to both parameter sets
+        $returnParams['transactionId'] = $transactionId;
+        $checkoutParams['transactionId'] = $transactionId;
+    
+        // Rebuild the returnUrl and checkoutUrl
+        $finalReturnUrl = strtok($returnUrl, '?') . '?' . http_build_query($returnParams, '', '&');  // Ensure & is used, not &amp;
+        $finalCheckoutUrl = strtok($checkoutUrl, '?') . '?' . http_build_query($checkoutParams, '', '&');
+    
+        // Append the returnUrl as a parameter to the checkoutUrl
+        $finalCheckoutUrl .= '&returnUrl=' . urlencode($finalReturnUrl);
+    
+        return $finalCheckoutUrl;
     }
 }
